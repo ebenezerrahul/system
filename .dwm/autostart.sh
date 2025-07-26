178bd13c54543db99a2cbab91cc0113b99ed33c7
ebenezerrahul <ebenezerrahul@gmail.com>
2025-07-27 00:53:28 +0530

    added config for scaling my displays

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -4,10 +4,15 @@ picom &
 
 xrandr --output eDP-1 --right-of HDMI-1 || exit 0
 
+xrandr --output HDMI-1 --scale 0.75x0.75 || exit 0
+
+xrandr --output eDP-1 --scale 1x1 || exit 0
+
 udiskie &
 
 xbindkeys &
 
 dwmblocks &
+
 
 wal -i /home/ebenezerrahul/Downloads/wallhaven-d68pdg_3840x2160.png -o /home/ebenezerrahul/.local/scripts/post_wal_script -a 50 || exit 0
